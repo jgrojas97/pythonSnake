20d07613122ee2b3072e56dd91e2189a2e135330
jgrojas97 <juangabrielrojas97@gmail.com>
2024-08-06 20:09:43 -0300

    Started the project

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import *
+
+
+class Snake():
+    def __init__(self):
+        pass
+
+
+def main():
+    # initialize the pygame engine
+    pygame.init()
+
+
+
+if __name__ == '__main__':
+    main()
